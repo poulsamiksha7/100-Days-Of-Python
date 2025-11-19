d53f41be47ab85c7031f550e5462ade1db4904e8
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-11-19 21:34:30 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,7 +0,0 @@
-#Writing your First Program
-
-print("Hello World!")
-
-#print is used to display output on screen or file
-#() The parenthesis contains the message we want to print
-#" " used to write a string(a sequence of charachters like text)
