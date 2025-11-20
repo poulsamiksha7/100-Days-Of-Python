7d8a6333a0a930f95bbf590f592f4f4d98e33ac8
Samiksha Dnyanoba Poul <poulsamiksha7@gmail.com>
2025-11-20 15:15:08 +0530

    Day 3: Understanding the Python Syntax

---
+++ Day-003/hello.py
@@ -0,0 +1,29 @@
+# Q1: Indentation Practice
+if 5 > 3:
+    print("5 is greater")
+
+# Q2: Single-Line Comment Practice
+a = 10
+b = 20
+sum = a + b       # Addition of a and b using the variable 'sum'
+print(sum)
+
+# Q3: Multi-Line Comment Practice
+"""
+Python is a dynamically-typed, object-oriented programming language.
+It was developed by Guido van Rossum.
+The ABC programming language influenced the design of Python.
+"""
+
+'''
+Indentation matters in Python because it shows which block of code
+belongs together. It represents nested blocks and improves readability.
+'''
+print("Python comments understood!")
+
+# Q4: Case Sensitivity Test
+name = "samiksha"
+Name = "SAMIKSHA"
+
+print(name)  # lowercase variable
+print(Name)  # uppercase variable (different variable because Python is case-sensitive)
